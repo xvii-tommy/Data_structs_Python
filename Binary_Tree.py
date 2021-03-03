888dfe6c9b59e3a30ed229920d37aa0fab2d58db
xvii-tommy <thomas.easterbrook97@gmail.com>
2021-03-03 16:30:18 +0000

    TE Binary tree search and insert functionality

--- Binary_Tree.py
+++ Binary_Tree.py
@@ -1,40 +1,39 @@
-class Node:
+class Binary_Tree_Node:
     def __init__(self, value=None):
         self.value = value
         self.left = None
         self.right = None
 
 
-class Binary_Tree:
-    def __init__(self):
-        self.root = None
+    def insertNode(self, insertval):
+        if self.value:
+            if insertval < self.value:
+                if self.left is None:
+                    self.left = Binary_Tree_Node(insertval)
+                else:
+                    self.left.insertNode(insertval)
+            elif insertval > self.value:
+                if self.right is None:
+                    self.right = Binary_Tree_Node(insertval)
+                else:
+                    self.right.insertNode(insertval)
+        else:
+            self.value = insertval
 
-    def SearchTree(self, searchval):
-        printval = self.root
-        while printval.value is not None:
-            print(printval.value)
-            if printval.value == searchval:
+    def search_tree(self, searchval):
+        if self.value is not None:
+            print(self.value)
+            if searchval == self.value:
+                print("The value" + searchval + "has been found")
                 return
-            elif printval.value > searchval:
-                printval = self.root.left
-
+            elif searchval < self.value:
+                self.left.search_tree(searchval)
             else:
-                printval = self.root.right
+                self.right.search_tree(searchval)
 
-        def addval(value):
-            pass
-
-        def dfsearch(value):
-            pass
-
-        def bfsearch(value):
-            pass
-
-
-bt = Binary_Tree()
-bt.root = Node(10)
-bt.root.left = Node(5)
-bt.root.right = Node(15)
-
-bt.SearchTree(15)
-
+root = Binary_Tree_Node(15)
+root.insertNode(13)
+root.insertNode(10)
+root.insertNode(17)
+root.insertNode(20)
+root.search_tree(20)
